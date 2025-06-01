be19834be5093bfad9325e9635186660330d3963
Piyush Jaipuriyar <piyushjaipuriyar@gmail.com>
2025-06-01 21:52:17 +0530

    feat: init

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# 📦 Changelog
+
+All notable changes to **File2FA** will be documented in this file.
+
+This project uses [Semantic Versioning](https://semver.org) and [Conventional Commits](https://www.conventionalcommits.org/) for automated changelog generation via [`semantic-release`](https://github.com/semantic-release/semantic-release).
+
+---
+
+## [Unreleased]
+
+Changes not yet released.
+
---
+++ release.config.js
@@ -0,0 +1,17 @@
+// release.config.js
+export default {
+    branches: ['main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/github',
+        '@semantic-release/git'
+    ],
+    preset: 'conventionalcommits',
+    changelogFile: 'CHANGELOG.md',
+    git: {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    },
+};
